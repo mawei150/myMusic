05240729a58c01ba23ac23fa2f4811876a8606d7
mawei <1501627568@qq.com>
2020-08-10 23:15:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mymusicandroid/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.mymusicandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author MW
+ * @date 2020/8/10
+ * <p>
+ * 描述：主页
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/mymusicandroid/activity/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.mymusicandroid.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mymusicandroid.R;
+
+/**
+ * @author MW
+ * @date 2020/8/10
+ * <p>
+ * 描述：引导界面
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
